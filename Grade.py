57cff71f1d8bbcf1f456fcba6008f6f56e207bb6
Deenoflife <45356327+Deenoflife@users.noreply.github.com>
2018-11-26 12:05:57 +0100

    Add files via upload

---
+++ Grade.py
@@ -0,0 +1,22 @@
+"""
+WRITE A CODE TO EXECUTE STUDENT GRADE:
+STUDENT GRADE CALCULATOR.
+"""
+
+A = 1-10
+B = 11-20
+C = 21-30
+D = 31-40
+E = 41-50
+
+if (A==11) :
+  print ("yes")
+elif (B==21):
+    print ("yes")
+elif (C==31):
+  print ("yes")
+elif (D==41):
+   print ("yes")
+else:
+    print ("no")
+    
